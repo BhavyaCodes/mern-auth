8959f46643b0d88245bedf0790add4d04c90d761
Bhavya <xbhavyatomar@gmail.com>
2021-05-12 19:41:20 +0530

    setup typescript and nodemon

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = process.env.PORT || 5000;
---
+++ server/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    required: true,
+    type: String,
+  },
+  lastName: {
+    required: true,
+    type: String,
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+});
