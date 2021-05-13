d1955a0d3b182151fb8e496fc65a97e4f1ac9cbe
Bhavya Tomar <58269749+BhavyaCodes@users.noreply.github.com>
2021-05-14 00:11:55 +0530

    Setup build scripts (#3)
    * add build and start script for server
    
    * add build script for client
    
    * create script to build client and frontend
    
    * add heroku-postbuild script

--- server/config.ts
+++ server/config.ts
@@ -1,7 +1,7 @@
 const config = {
   dbUrl:
     process.env.NODE_ENV === "production"
-      ? ""
+      ? process.env.DB_URL
       : "mongodb://localhost:27017/mern-auth",
   port: process.env.PORT || 5000,
   cookieKey: process.env.COOKIE_KEY,
